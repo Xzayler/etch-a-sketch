f4a755a4cd3514e4463b8dffd30f7e173a514e68
Xzayler <csak.mityu@gmail.com>
2022-10-20 22:41:20 +0200

    Add colour on grid box hover

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const gridContainer = document.querySelector(`#grid-container`);
 
+function paintBox(e) {
+  e.target.classList.add("painted");
+}
+
 let gridBoxes = [];
 for (let i = 0; i < 16; i++) {
   gridBoxes[i] = document.createElement('div');
   gridBoxes[i].classList.add("grid-box");
+  gridBoxes[i].addEventListener('mouseenter', paintBox);
   gridContainer.appendChild(gridBoxes[i]);
 }
