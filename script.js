a2f074f7d1a7ec8d2dd3fbd3c319a0402f560e60
Xzayler <csak.mityu@gmail.com>
2022-10-21 14:04:25 +0200

    Add reset function to clear grid of color

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ function paintBox(e) {
   e.target.classList.add("painted");
 }
 
+function resetGrid() {
+  for (const box of gridContainer.children) {
+    box.classList.remove("painted");
+  }
+}
+
 function shrinkGrid(difference) {
-  console.log("Boxes to remove: " + difference);
   for (let i = 0; i < difference; i++) {
     gridContainer.removeChild(gridContainer.lastChild);
   }
 }
 
 function expandGrid(difference) {
-  console.log(`Boxes to add: ${difference}`);
   let gridBoxes = [];
   for (let i = 0; i < difference; i++) {
     gridBoxes[i] = document.createElement('div');
@@ -27,8 +31,6 @@ function resizeGrid() {
   if (isNaN(sideSize)) {
     alert("Please give a valid number");
   }
-  console.log("Number of boxes: " + gridContainer.children.length);
-  console.log("New sideSize: " + sideSize);
   if (sideSize ** 2 < gridContainer.children.length) { //if sideSize > current side length
     let squaresDiff = gridContainer.children.length - (sideSize ** 2)
     shrinkGrid(squaresDiff); 
@@ -46,10 +48,10 @@ function resizeGrid() {
       gridContainerRules = stylesheet.cssRules[i];
     }
   }
+
+  resetGrid();
   
   gridContainerRules.style.setProperty('grid-template', `repeat(${sideSize}, 1fr) / repeat(${sideSize}, 1fr)`);
-
-  console.log("--------------");
 }
 expandGrid(16);
 
